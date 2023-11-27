6f0b2466d4a0fcb7c549cccf65069b14bd89b998
vtran10 <victortran012@gmail.com>
2023-11-27 10:18:43 -0700

    added nike proj

---
+++ nike/src/components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductcard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, ShoeCard, Nav, PopularProductCard, ServiceCard, ReviewCard };
